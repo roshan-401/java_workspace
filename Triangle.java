462a6b664744a6e0718cd1468ac677d50e709ace
roshan <amalazimroshan@gmail.com>
2023-06-11 17:34:41 +0530

    directory restructure

--- Triangle.java
+++
@@ -1,9 +0,0 @@
-package graphics;
-public class Triangle implements Graphics{
-    public double  area(double  l, double  b) {
-        return l*b*0.5;
-    }
-    public double  perimeter(double  l, double  b) {
-        return l+b + Math.sqrt((l*l)+(b*b));
-    }
-}
---
+++ qns1/graphics/Graphics.java
@@ -0,0 +1,6 @@
+package graphics;
+interface Graphics{
+    double  area(double  l,double  b);
+    double  perimeter(double  l,double  b);
+}
+
